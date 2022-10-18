0279e2633f61b4ab744da1e3ef54f2d122ba1506
Oscar Soler <oscarsoler@MacBook-Air-de-Oscar.local>
2022-10-17 21:54:36 -0500

    flash messages

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -2,10 +2,11 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
 	connect() {
-		setTimeout(() => {
-			this.element.remove();
-		}, 9000);
+		// setTimeout(() => {
+		// 	this.element.remove();
+		// }, 9000);
 	}
+
 	dismiss() {
 		this.element.remove();
 	}
